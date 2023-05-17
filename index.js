42d767e51c6e5b0c1d3c3b75c6c242f4abf7991c
Danilo <danilorocco2004@gmail.com>
2023-05-17 23:29:24 +0200

    web

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express()
 const PORT = 8080
 const routesProducts = require ("./routes/products")
 const routesCarts = require ("./routes/carts")
+const handlebars = require ("express-handlebars")
 
+app.engine("handlebars", handlebars.engine())
+app.set("views", __dirname+"/views")
+app.set("view engine", "handlebars")
 app.use(express.json()) 
 app.use(express.urlencoded({extended:true}))
 
@@ -18,8 +22,23 @@ app.use("/static", express.static(__dirname + "/public"))
 app.use("/products",routesProducts)
 app.use("/carts",routesCarts) 
 
-
-
+app.get("/", ( req, res )=>{
+    let user = {
+        name: "Israel"
+    }
+    res.render("index" , user)
+})
+app.get("/testHand", ( req, res )=>{
+    let user = {
+        name: "Danilo",
+        lastName: "Laura",
+        role: "user"
+    }
+    res.render("user", {
+        user: user,
+        IsAdmin: user.role === "admin"
+    })
+})
 
 
 app.listen(PORT, ()=>{
